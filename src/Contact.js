a6a8d0e9159bd3dab141398be11dce5ac1981f56
Sophia <sophia852686@gmail.com>
2022-07-13 22:29:21 +1000

    create login form

---
+++ src/Contact.js
@@ -0,0 +1,53 @@
+
+import React, { Component } from 'react';
+import './App.css';
+
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      password: 'swordfish',
+      authorized: false
+    };
+    this.authorize = this.authorize.bind(this);
+  }
+
+  //check wheather password is equal to ‘swordfish’
+  authorize(e) {
+    const password = e.target.querySelector(
+      'input[type="password"]').value;
+    const auth = password == this.state.password;
+    this.setState({
+      authorized: auth
+    });
+  }
+
+  render() {
+    const login = (
+      <form action="#" onSubmit={this.authorize}>
+        <input type="password" palceholder="Password" />
+        <input type="submit"
+        />
+      </form>);
+
+    const contactInfo = (
+      <ul>
+        <li>
+          client@example.com
+        </li>
+        <li>
+          555.555.5555
+        </li>
+      </ul>
+    )
+
+    return (
+      <div id="authorization">
+        <h1>{this.state.authorized ? 'Contact' : 'Enter the Password'}</h1>
+        {this.state.authorized ? contactInfo : login}
+      </div>
+    );
+  }
+}
+
+export default Contact;
